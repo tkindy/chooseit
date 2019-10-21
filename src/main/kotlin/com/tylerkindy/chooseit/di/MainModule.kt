c58b0e936346b9590422fbeef79e3ef6a6c5310d
Tyler Kindy <tylerkindy@gmail.com>
2019-10-21 00:28:52 -0400

    Remove old routes

--- src/main/kotlin/com/tylerkindy/chooseit/di/MainModule.kt
+++ src/main/kotlin/com/tylerkindy/chooseit/di/MainModule.kt
@@ -1,11 +1,8 @@
 package com.tylerkindy.chooseit.di
 
-import com.tylerkindy.chooseit.data.RoomManager
-import com.tylerkindy.chooseit.model.Rooms
 import com.tylerkindy.chooseit.routes.RoomRoutes
 import dagger.Module
 import dagger.Provides
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CORS
 import io.ktor.features.CallLogging
@@ -13,24 +10,18 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Locations
-import io.ktor.response.respond
-import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.routing.routing
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import me.liuwj.ktorm.dsl.insert
-import java.nio.ByteBuffer
-import java.util.*
 
 @Module
 class MainModule {
     @KtorExperimentalLocationsAPI
     @Provides
     fun provideServer(
-        roomRoutes: RoomRoutes,
-        roomManager: RoomManager
+        roomRoutes: RoomRoutes
     ): ApplicationEngine {
         return embeddedServer(Netty, port = System.getProperty("server.port").toInt()) {
             install(ContentNegotiation) {
@@ -43,12 +34,7 @@ class MainModule {
             }
 
             routing {
-                route("/room", roomRoutes.buildRoomRoute)
                 route("/rooms", roomRoutes.buildRoomRoute)
-                post("/new-room") {
-                    val id = roomManager.makeNewRoom()
-                    call.respond(id)
-                }
             }
         }
     }
