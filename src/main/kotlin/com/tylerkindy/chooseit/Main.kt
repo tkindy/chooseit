8bd325f3692d93beffa9d7f5b216218ee6bee7ea
Tyler Kindy <tylerkindy@gmail.com>
2019-10-26 18:04:51 -0400

    Add warning annotations

--- src/main/kotlin/com/tylerkindy/chooseit/Main.kt
+++ src/main/kotlin/com/tylerkindy/chooseit/Main.kt
@@ -1,11 +1,11 @@
 package com.tylerkindy.chooseit
 
 import com.tylerkindy.chooseit.di.DaggerAppComponent
-import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.server.engine.ApplicationEngine
+import io.ktor.util.KtorExperimentalAPI
 import me.liuwj.ktorm.database.Database
 
-@KtorExperimentalLocationsAPI
+@KtorExperimentalAPI
 fun main() {
     Database.connect(
         System.getenv("JDBC_DATABASE_URL"),
--- src/main/kotlin/com/tylerkindy/chooseit/di/AppComponent.kt
+++ src/main/kotlin/com/tylerkindy/chooseit/di/AppComponent.kt
@@ -2,7 +2,9 @@ package com.tylerkindy.chooseit.di
 
 import dagger.Component
 import io.ktor.server.engine.ApplicationEngine
+import io.ktor.util.KtorExperimentalAPI
 
+@KtorExperimentalAPI
 @Component(modules = [
     MainModule::class
 ])
